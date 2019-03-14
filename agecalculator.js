d5eb1e35460500801765eed689aa8ec60f8c4856
SMLukwiya <sundaymorganl@gmail.com>
2019-03-14 15:56:51 +0300

    Delete agecalculator.js

--- agecalculator.js
+++
@@ -1,8 +0,0 @@
-var birthDate= '08/06/1995'
-birthDate = new Date(birthDate);
-
-var otherDate = new Date();
-
-var years = (otherDate.getFullYear() - birthDate.getFullYear());
-
-console.log("You are "+ years+ " years old")
